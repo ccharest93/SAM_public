093740d7bb43fab61a26503f62154c5f238884f9
Maatra <ccharest93@hotmail.com>
2023-07-08 11:59:03 +0100

    reformating

---
+++ SAMPredictor.py
@@ -0,0 +1,159 @@
+
+import numpy as np
+import torch
+
+from SAM import SAM
+
+from typing import Optional, Tuple
+
+from SAM_Transforms import ResizeLongestSide
+
+
+class SamPredictor:
+    def __init__(
+        self,
+        sam_model: SAM,
+    ) -> None:
+        """
+        Uses SAM to calculate the image embedding for an image, and then
+        allow repeated, efficient mask prediction given prompts.
+
+        Arguments:
+          sam_model (Sam): The model to use for mask prediction.
+        """
+        super().__init__()
+        self.model = sam_model
+        self.transform = ResizeLongestSide(sam_model.image_encoder.img_size)
+        self.reset_image()
+
+    @torch.no_grad()
+    def set_image(
+        self,
+        image: np.ndarray,
+    ) -> None:
+        """
+        Calculates the image embeddings for the provided image, allowing
+        masks to be predicted with the 'predict' method.
+
+        Arguments:
+          image (np.ndarray): The image for calculating masks. Expects an
+            image in HWC uint8 format, with pixel values in [0, 255].
+          image_format (str): The color format of the image, in ['RGB', 'BGR'].
+        """
+
+        self.reset_image()
+
+        # Transform the image to the form expected by the model
+        input_image = self.transform.apply_image(image)
+        input_image_torch = torch.as_tensor(input_image)
+        input_image_torch = input_image_torch.permute(2, 0, 1).contiguous()[None, :, :, :]
+        input_image = self.model.preprocess(input_image_torch)
+
+        #GPU context switch to set features
+        self.features = self.model.image_encoder(input_image)
+
+        # Set various attributes
+        self.original_size = image.shape[:2]
+        self.input_size = tuple(input_image_torch.shape[-2:])
+        self.is_image_set = True
+
+    @torch.no_grad()
+    def predict(
+        self,
+        point_coords: Optional[np.ndarray] = None,
+        point_labels: Optional[np.ndarray] = None,
+        box: Optional[np.ndarray] = None,
+        mask_input: Optional[np.ndarray] = None,
+        multimask_output: bool = True,
+        return_logits: bool = False,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Predict masks for the given input prompts, using the currently set image.
+
+        Arguments:
+          point_coords (np.ndarray or None): A Nx2 array of point prompts to the
+            model. Each point is in (X,Y) in pixels.
+          point_labels (np.ndarray or None): A length N array of labels for the
+            point prompts. 1 indicates a foreground point and 0 indicates a
+            background point.
+          box (np.ndarray or None): A length 4 array given a box prompt to the
+            model, in XYXY format.
+          mask_input (np.ndarray): A low resolution mask input to the model, typically
+            coming from a previous prediction iteration. Has form 1xHxW, where
+            for SAM, H=W=256.
+          multimask_output (bool): If true, the model will return three masks.
+            For ambiguous input prompts (such as a single click), this will often
+            produce better masks than a single prediction. If only a single
+            mask is needed, the model's predicted quality score can be used
+            to select the best mask. For non-ambiguous prompts, such as multiple
+            input prompts, multimask_output=False can give better results.
+          return_logits (bool): If true, returns un-thresholded masks logits
+            instead of a binary mask.
+
+        Returns:
+          (np.ndarray): The output masks in CxHxW format, where C is the
+            number of masks, and (H, W) is the original image size.
+          (np.ndarray): An array of length C containing the model's
+            predictions for the quality of each mask.
+          (np.ndarray): An array of shape CxHxW, where C is the number
+            of masks and H=W=256. These low resolution logits can be passed to
+            a subsequent iteration as mask input.
+        """
+        if not self.is_image_set:
+            raise RuntimeError("An image must be set with .set_image(...) before mask prediction.")
+
+        # Transform input prompts
+        point_coords, point_labels, box, mask_input = None, None, None, None
+        if point_coords is not None:
+            assert (
+                point_labels is not None
+            ), "point_labels must be supplied if point_coords is supplied."
+            point_coords = self.transform.apply_coords(point_coords, self.original_size)
+            point_coords = torch.as_tensor(point_coords[None, :, :], dtype=torch.float)
+            point_labels = torch.as_tensor(point_labels[None, :], dtype=torch.int)
+            points = (point_coords, point_labels)
+        else:
+            points = None
+
+        if box is not None:
+            box = self.transform.apply_boxes(box, self.original_size)
+            box = torch.as_tensor(box[None, :], dtype=torch.float)
+
+        if mask_input is not None:
+            mask_input = torch.as_tensor(mask_input[None, :, :, :], dtype=torch.float)
+
+
+        #GPU context switch to set embeddings
+        sparse_embeddings, dense_embeddings = self.model.prompt_encoder(
+            points=points,
+            boxes=box,
+            masks=mask_input,
+        )
+
+        #GPU context switch to decode masks and upscale them
+        low_res_masks, iou_predictions = self.model.mask_decoder(
+            image_embeddings=self.features,
+            image_pe=self.model.prompt_encoder.get_dense_pe(),
+            sparse_prompt_embeddings=sparse_embeddings,
+            dense_prompt_embeddings=dense_embeddings,
+            multimask_output=multimask_output,
+        )
+        masks = self.model.postprocess_masks(low_res_masks, self.input_size, self.original_size)
+
+        #threshold masks
+        if not return_logits:
+            masks = masks > self.model.mask_threshold
+
+        masks_np = masks[0].detach().cpu().numpy()
+        iou_predictions_np = iou_predictions[0].detach().cpu().numpy()
+        low_res_masks_np = low_res_masks[0].detach().cpu().numpy()
+        return masks_np, iou_predictions_np, low_res_masks_np
+
+    def reset_image(self) -> None:
+        """Resets the currently set image."""
+        self.is_image_set = False
+        self.features = None
+        self.orig_h = None
+        self.orig_w = None
+        self.input_h = None
+        self.input_w = None
---
+++ display_img.py
@@ -0,0 +1,66 @@
+from SAMPredictor import SamPredictor
+from SAM import SAM
+import gradio as gr
+from PIL import Image
+import numpy as np
+def show_mask(mask, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30, 144, 255])
+    h, w = mask.shape[-2:]
+    return mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+
+def img_clicked(evt: gr.SelectData, pil_img):
+    input_point = np.array([evt.index])
+    input_label = np.array([1])
+    masks, scores, logits = predictor.predict(
+    point_coords=input_point,
+    point_labels=input_label,
+    multimask_output=True,
+    )
+    return [(pil_img*0.4 + show_mask(mask)*0.6).astype(np.uint8) for mask in masks]
+
+
+
+def process(np_img):
+    prep_img = predictor.set_image(np_img)
+    # input_point = np.array([[500, 375]])
+    # input_label = np.array([1])
+    # masks, scores, logits = predictor.predict(
+    # point_coords=input_point,
+    # point_labels=input_label,
+    # multimask_output=True,
+    # )
+    return np_img, [prep_img]
+if __name__ == "__main__":
+    #GLOBAL VARIABLES
+    model = SAM("vit_l")
+    predictor = SamPredictor(model)
+    # #GRADIO
+    block = gr.Blocks().queue()
+    with block:
+        #GRADIO LAYOUT
+        with gr.Row():
+            gr.Markdown("## SAM predictor")
+        with gr.Row():
+            with gr.Column():
+                pil_img = gr.Image(source='upload', type="numpy")
+            with gr.Column():
+                gallery = gr.Gallery()
+        #GRADIO EVENTS
+        pil_img.select(fn=img_clicked, inputs=[pil_img], outputs=[gallery])
+        pil_img.upload(fn=process, inputs=[pil_img], outputs=[pil_img, gallery])
+    block.launch()
+
+
+    np_img = np.array(Image.open(r"C:\Users\cchar\Downloads\Horses\R (2).jpg"))
+    input_point = np.array([[500, 375]])
+    input_label = np.array([1])
+    predictor.set_image(np_img)
+    masks, scores, logits = predictor.predict(
+    point_coords=input_point,
+    point_labels=input_label,
+    multimask_output=True,
+    )
+    print(scores)
